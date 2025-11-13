bf650e318aa03bcbcab60bf807ed71bf3b0347b2
kabaww <josh@krokismedia.com>
2025-11-14 07:45:25 +0800

    fixes for deprecated shit

--- lua/custom/plugins/nvim-svelte.snippets.lua
+++ lua/custom/plugins/nvim-svelte.snippets.lua
@@ -1,9 +1,10 @@
-return {
-  'nvim-svelte/nvim-svelte-snippets',
-  dependencies = 'L3MON4D3/LuaSnip',
-  opts = {
-    enabled = true, -- Enable/disable snippets globally
-    auto_detect = true, -- Only load in SvelteKit projects
-    prefix = 'kit', -- Prefix for TypeScript snippets (e.g., kit-load)
-  },
-}
+-- return {
+--   'nvim-svelte/nvim-svelte-snippets',
+--   dependencies = 'L3MON4D3/LuaSnip',
+--   opts = {
+--     enabled = true, -- Enable/disable snippets globally
+--     auto_detect = true, -- Only load in SvelteKit projects
+--     prefix = 'kit', -- Prefix for TypeScript snippets (e.g., kit-load)
+--   },
+-- }
+return {}
